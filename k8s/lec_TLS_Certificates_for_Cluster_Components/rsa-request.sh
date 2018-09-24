fcec4bb1d374cc6223f6423a81821cfabbda641a
gky360 <gky360@gmail.com>
2018-09-24 14:57:41 +0000

    lec_TLS_Certificates_for_Cluster_Components

---
+++ k8s/lec_TLS_Certificates_for_Cluster_Components/rsa-request.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+./easyrsa --subject-alt-name="IP:${MASTER_IP},"\
+"IP:${MASTER_IP},"\
+"DNS:kubernetes,"\
+"DNS:kubernetes.default,"\
+"DNS:kubernetes.default.svc,"\
+"DNS:kubernetes.default.svc.cluster,"\
+"DNS:kubernetes.default.svc.cluster.local" \
+--days=10000 \
+build-server-full server nopass
